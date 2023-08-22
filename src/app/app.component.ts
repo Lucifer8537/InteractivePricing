37b80eb28d27e51b0b25c82ffe2d4f173d693f2a
navodit <navodit50@gmail.com>
2023-08-22 20:04:15 +0530

    First Push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './mobile.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  right = 50;
   title = 'interactivePricing';
+  range = 50;
+  style!: string | null;
+  width!: number;
+  mobileView!: boolean;
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.width = screen.width;
+    this.mobileView = this.width < 679 || window.innerWidth < 679;
+  }
+
+  ngOnInit(): void {
+    this.onResize();
+  }
+  onRangeChange = () => {
+    this.style =
+      'linear-gradient(to right, hsl(174, 77%, 80%) ' +
+      this.range +
+      '%, transparent ' +
+      0 +
+      '%, hsl(224, 65%, 95%) ' +
+      (100 - this.range) +
+      '%)';
+  };
 }
